63600da26a3272a1b0caeb66d0326026d6dd6e75
Gerald Ke <gerald.nke@gmail.com>
2013-09-16 05:41:47 +0800

    Added jquery and a few tweaks

--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,43 @@
-window.onload = function() {
+$(document).ready(function() {
     var messages = [];
     var socket = io.connect('http://localhost:3789');
     var field = document.getElementById("message");
     var sendButton = document.getElementById("send");
-    var content = document.getElementById("messages");
+    var content = document.getElementById("content");
+    var name = document.getElementById("name");
+
+    function sendMessage() {
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+            field.value = "";
+        }
+    }
 
     socket.on('message', function (data) {
         if(data.message) {
-            messages.push(data.message);
+            messages.push(data);
             var html = '';
             for(var i=0; i<messages.length; i++) {
-                html += messages[i] + '<br />';
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
             }
             content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
         } else {
             console.log("There is a problem:", data);
         }
     });
 
     sendButton.onclick = function() {
-        var text = field.value;
-        socket.emit('send', { message: text });
+        sendMessage();
     };
-}
+
+    $("#message").keyup(function(e) {
+        if(e.keyCode == 13) {
+            sendMessage();
+        }
+    });
+});
