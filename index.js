5304a007742a3ebe0e5865a743c488775eb06a2a
Gerald Ke <gerald.nke@gmail.com>
2013-10-13 02:25:27 +0800

    Fixed code style

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var port = 1337;
+var port = 1234;
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
-app.get("/", function(req, res){
-    res.render("page");
+app.get('/', function(req, res){
+    res.render('page');
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'Welcome to GChat' });
+    socket.emit('message', { message: 'Welcome to the chat room!' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
--- public/chat.js
+++ public/chat.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
+    var ip = '127.0.0.1';
+    var port = '1234';
+    var socket = io.connect('http://' + ip + ':' + port);
+
     var messages = [];
-    var socket = io.connect('http://192.241.186.57:1337');
-    var field = document.getElementById("message");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
-    var name = document.getElementById("name");
+    var field = $('#message');
+    var sendButton = $('#send');
+    var content = $('#content');
+    var name = $('#name');
 
     function sendMessage() {
-        if(name.value == "") {
-            alert("Please type your name!");
+        if(name.value == '') {
+            alert('Please enter your name.');
         } else {
             var text = field.value;
             socket.emit('send', { message: text, username: name.value });
-            field.value = "";
+            field.value = '';
         }
     }
 
@@ -27,15 +30,15 @@ $(document).ready(function() {
             content.innerHTML = html;
             content.scrollTop = content.scrollHeight;
         } else {
-            console.log("There is a problem:", data);
+            console.log('Error encountered', data);
         }
     });
 
-    sendButton.onclick = function() {
+    sendButton.click(function() {
         sendMessage();
-    };
+    })
 
-    $("#message").keyup(function(e) {
+    $('#message').keyup(function(e) {
         if(e.keyCode == 13) {
             sendMessage();
         }
