a53959a4805b045dbacc482d8939b9f3ad5bff0b
Gerald Ke <gerald.nke@gmail.com>
2013-10-13 02:48:03 +0800

    Change to use existing selector

--- public/chat.js
+++ public/chat.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         sendMessage();
     })
 
-    $('#message').keyup(function(e) {
+    field.keyup(function(e) {
         if(e.keyCode == 13) {
             sendMessage();
         }
